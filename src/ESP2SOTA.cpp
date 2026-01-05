2cc34ca00ac71ef805aa56eed8f658396a6bc3e1
walulyafrancis <wfrancis@binarique.com>
2026-01-05 15:51:34 +0300

    Fixed fail to send http 200 after upload due to fast restart

--- src/ESP2SOTA.cpp
+++ src/ESP2SOTA.cpp
@@ -1,28 +1,32 @@
 #include "ESP2SOTA.h"
 #include "index_html.h"
-//Class constructor
-ESP2SOTAClass::ESP2SOTAClass(){
-
+// Class constructor
+ESP2SOTAClass::ESP2SOTAClass()
+{
 }
 
 #if defined(ESP8266)
-  void ESP2SOTAClass::begin(ESP8266WebServer *server){
+void ESP2SOTAClass::begin(ESP8266WebServer *server)
+{
 #elif defined(ESP32)
-  void ESP2SOTAClass::begin(WebServer *server){
+void ESP2SOTAClass::begin(WebServer *server)
+{
 #endif
   _server = server;
-  //Returns index.html page
-  _server->on("/update", HTTP_GET, [&]() {
+  // Returns index.html page
+  _server->on("/update", HTTP_GET, [&]()
+              {
     _server->sendHeader("Connection", "close");
-    _server->send(200, "text/html", indexHtml);
-  });
+    _server->send(200, "text/html", indexHtml); });
 
   /*handling uploading firmware file */
-  _server->on("/update", HTTP_POST, [&]() {
+  _server->on("/update", HTTP_POST, [&]()
+              {
     _server->sendHeader("Connection", "close");
     _server->send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
-  }, [&]() {
+    delay(1000);
+    ESP.restart(); }, [&]()
+              {
     HTTPUpload& upload = _server->upload();
     if (upload.status == UPLOAD_FILE_START) {
       Serial.printf("Update: %s\n", upload.filename.c_str());
@@ -40,8 +44,7 @@ ESP2SOTAClass::ESP2SOTAClass(){
       } else {
         Update.printError(Serial);
       }
-    }
-  });
+    } });
 }
 
 ESP2SOTAClass ESP2SOTA;
